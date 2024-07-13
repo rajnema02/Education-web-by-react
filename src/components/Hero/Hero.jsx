bb74b5d02ba30f7b19094afadfc4d81153446540
rajnema02 <rajnema02@gmail.com>
2024-07-13 15:20:21 +0530

    first change

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero.css'
 const Hero = () => {
   return (
-    <div className='hero container' id='hero'>
+    <div className='hero container'>
       <div className="hero-text">
         <h1>This is education website</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime officiis accusantium pariatur, eaque sequi molestiae magnam magni labore.</p>
